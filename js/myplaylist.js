c767a1d7e99895e814cd2e2daf2010c8b29a76c3
Programador Web <developerdombosco@gmail.com>
2017-06-22 16:39:25 -0300

    upload

---
+++ js/myplaylist.js
@@ -0,0 +1,83 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01_Battleme.mp3',
+        title:'Hey Hey My My',
+        artist:'Battleme',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:00',
+        cover:'mix/thumb.jpg'
+    },
+
+    {
+        mp3:'mix/02_Forever.mp3',
+        title:'Forever Young',
+        artist:'Audra Mae',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:30',
+        cover:'mix/thumb.jpg'
+    },
+    {
+        mp3:'mix/03_Buffalo.mp3',
+        title:'The House of the Rising Sun',
+        artist:'The White Buffalo',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:10',
+        cover:'mix/thumb.jpg'
+    },
+    {
+        mp3:'mix/06_los.mp3',
+        title:'A Flor',
+        artist:'Los Hermanos',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:10',
+        cover:'mix/thumb2.jpg'
+    },
+    {
+        mp3:'mix/07_supercombo-piloto.mp3',
+        title:'Piloto Automatico',
+        artist:'Supercombo',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:10',
+        cover:'mix/thumb3.jpg'
+    },
+    {
+        mp3:'mix/08_criolo-ainda-ha-temp.mp3',
+        title:'Ainda Ha Tempo',
+        artist:'Criolo',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:10',
+        cover:'mix/thumb4.jpg'
+    },
+    {
+        mp3:'mix/09_slipknot-snuff.mp3',
+        title:'Snuff',
+        artist:'Slipknot',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:10',
+        cover:'mix/thumb5.jpg'
+    }
+    
+];
